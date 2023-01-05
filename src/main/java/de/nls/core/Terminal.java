a6c1a5de893e8c7d7df5abc0557fe4938303fe8f
Benjamin Schmid <bene.schmid@gmail.com>
2023-01-05 15:20:22 +0100

    Add some more terminal types:
    - Letter
    - Whitespace
    - CharacterClass

--- src/main/java/de/nls/core/Terminal.java
+++ src/main/java/de/nls/core/Terminal.java
@@ -1,5 +1,8 @@
 package de.nls.core;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public abstract class Terminal extends Symbol {
 
 	public Terminal(String symbol) {
@@ -76,6 +79,167 @@ public abstract class Terminal extends Symbol {
 		@Override
 		public String toString() {
 			return "'" + getSymbol() + "'";
+		}
+	}
+
+	public static class Letter extends Terminal {
+		public Letter() {
+			super("letter");
+		}
+
+		@Override
+		public Matcher matches(Lexer lexer) {
+			int pos = lexer.getPosition();
+			if(lexer.isAtEnd())
+				return new Matcher(ParsingState.END_OF_INPUT, pos, "");
+			char c = lexer.peek();
+			if(Character.isLetter(c))
+				return new Matcher(ParsingState.SUCCESSFUL, pos, Character.toString(c));
+			return new Matcher(ParsingState.FAILED, pos, "");
+		}
+	}
+
+	public static class Whitespace extends Terminal {
+		private Whitespace() {
+			super("whitespace");
+		}
+
+		@Override
+		public Matcher matches(Lexer lexer) {
+			int pos = lexer.getPosition();
+			if(lexer.isAtEnd())
+				return new Matcher(ParsingState.END_OF_INPUT, pos, "");
+			char c = lexer.peek();
+			if(c == ' ' || c == '\t')
+				return new Matcher(ParsingState.SUCCESSFUL, pos, Character.toString(c));
+			return new Matcher(ParsingState.FAILED, pos, "");
+		}
+	}
+
+	public static class CharacterClass extends Terminal {
+
+		private final Ranges ranges;
+
+		public CharacterClass(String pattern) {
+			super(pattern);
+			StringBuilder b = new StringBuilder(pattern.trim());
+			if(b.charAt(0) != '[' || b.charAt(b.length() - 1) != ']')
+				throw new RuntimeException("Wrong character class format: " + pattern);
+
+			int start = 1;
+			int end = b.length() - 2;
+
+			boolean negated = b.charAt(1) == '^';
+			if(negated)
+				start++;
+
+			ranges = new Ranges(negated);
+
+			if(b.charAt(start) == '-') {
+				ranges.add(new SingleCharacterRange('-'));
+				start++;
+			}
+			if(b.charAt(end) == '-') {
+				ranges.add(new SingleCharacterRange('-'));
+				end--;
+			}
+
+			int idx = start;
+			while(idx <= end) {
+				int nIdx = idx + 1;
+				char c = b.charAt(idx);
+				if(nIdx <= end && b.charAt(nIdx) == '-') {
+					char u = b.charAt(idx + 2);
+					if(c == '-' || u == '-')
+						throw new RuntimeException("Wrong character class format: " + pattern);
+					ranges.add(new CharacterRange(c, u));
+					idx = idx + 3;
+				}
+				else {
+					ranges.add(new SingleCharacterRange(c));
+					idx++;
+				}
+			}
+		}
+
+		@Override
+		public Matcher matches(Lexer lexer) {
+			int pos = lexer.getPosition();
+			if(lexer.isAtEnd())
+				return new Matcher(ParsingState.END_OF_INPUT, pos, "");
+			char c = lexer.peek();
+			if(ranges.checkCharacter(c))
+				return new Matcher(ParsingState.SUCCESSFUL, pos, Character.toString(c));
+			return new Matcher(ParsingState.FAILED, pos, "");
+		}
+	}
+
+	private static class CharacterRange {
+		int lower;
+		int upper;
+
+		public CharacterRange(int lower, int upper) {
+			this.lower = lower;
+			this.upper = upper;
+		}
+
+		public boolean checkCharacter(int i) {
+			return i >= lower && i <= upper;
+		}
+
+		public boolean equals(Object o) {
+			if(!o.getClass().equals(getClass()))
+				return false;
+			CharacterRange c = (CharacterRange) o;
+			return lower == c.lower && upper == c.upper;
+		}
+	}
+
+	private static class SingleCharacterRange extends CharacterRange {
+		int number;
+
+		public SingleCharacterRange(int number) {
+			super(0, 0);
+			this.number = number;
+		}
+
+		@Override
+		public boolean checkCharacter(int i) {
+			return i == number;
+		}
+	}
+
+	private static class Ranges {
+
+		private final ArrayList<CharacterRange> ranges = new ArrayList<>();
+
+		private final boolean negated;
+
+		public Ranges(boolean negated) {
+			this.negated = negated;
+		}
+
+		public void add(CharacterRange range) {
+			ranges.add(range);
+		}
+
+		public boolean checkCharacter(int i) {
+			for(CharacterRange range : ranges) {
+				boolean check = range.checkCharacter(i);
+				if(!negated && check)
+					return true;
+				if(negated && check)
+					return false;
+			}
+			return negated;
+		}
+
+		public boolean equals(Object o) {
+			if(!o.getClass().equals(this.getClass()))
+				return false;
+			HashSet<CharacterRange> ts = new HashSet<>(ranges);
+			HashSet<CharacterRange> os = new HashSet<>(((Ranges)o).ranges);
+			return ts.equals(os);
 		}
 	}
 }
